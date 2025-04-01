9db98d565bfef743dfbbeb3936107c5a6ff0cd75
Carrington Muleya <79579279+Carrington-dev@users.noreply.github.com>
2025-04-01 08:02:27 +0200

    Create test_api.py
    Co-Authored-By: Stemgon <165911512+stemgon@users.noreply.github.com>
    Co-Authored-By: Carrington Muleya <79579279+carrington-dev@users.noreply.github.com>

---
+++ tests/test_api.py
@@ -0,0 +1,21 @@
+# Test app
+
+from src import app
+from pytest import fixture
+
+@fixture
+def client():
+    with app.test_client() as client:
+        yield client
+
+def test_home(client):
+    response = client.get('/api/home')
+    assert response.status_code == 200
+    assert response.get_json() == {'message': 'Hi Carrington, my name is Wonderful'}
+
+def test_home(client):
+    response = client.get('/api/about')
+    assert response.status_code == 200
+    assert response.get_json() == {
+        "message": "About Us"
+    }
