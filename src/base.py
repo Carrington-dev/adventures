90898af0a550c0958d808621250bd6c136f6eb20
Carrington Muleya <79579279+Carrington-dev@users.noreply.github.com>
2025-04-02 16:18:29 +0200

    Update base.py
    Co-Authored-By: Stemgon <165911512+stemgon@users.noreply.github.com>
    Co-Authored-By: Carrington Muleya <79579279+carrington-dev@users.noreply.github.com>

--- src/base.py
+++ src/base.py
@@ -2,8 +2,14 @@ from flask import Flask
 from flasgger import Swagger
 
 app = Flask(__name__, static_url_path="/static", static_folder="../static", template_folder='../templates')
+
+app.config['SWAGGER'] = {
+    'title': 'This is an adventure API',
+    'uiversion': 3,  # Use Swagger UI version 3
+    'description': 'This is a simple adventure API with basic endpoints',
+    'version': '1.0.0'
+}
+
 swagger = Swagger(app)  # Initialize Swagger
-
-
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///mydatabase.db'  # Using SQLite
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False  # Optional but recommended
