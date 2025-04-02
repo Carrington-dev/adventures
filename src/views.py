1e22b73a62007f19e03de7561477b922c04bd1d2
Carrington Muleya <79579279+Carrington-dev@users.noreply.github.com>
2025-04-02 11:36:54 +0200

    Update views.py
    Co-Authored-By: Stemgon <165911512+stemgon@users.noreply.github.com>
    Co-Authored-By: Carrington Muleya <79579279+carrington-dev@users.noreply.github.com>

--- src/views.py
+++ src/views.py
@@ -5,12 +5,12 @@ from src import app
 
 @app.context_processor
 def inject_globals():
-    company = "Thrifty Adventures"
+    company = "Elite Adventures"
     return {
         "company": company,
         "year": 2025,
         "phone": '+27 (0) 78 735 2242',
-        "email": 'hello@thriftyadventures.co.za',
+        "email": 'hello@eliteadventures.co.za',
         "address": "395 Francis Baard Street</p><p>Pretoria Central, 0001/2</p><p>South Africa",
         "copyright_notice": f"© {datetime.now().year} { company }. All rights reserved.",
         "copyright": f"""© <span>{datetime.now().year}</span><strong class="px-1 sitename">{ company }.</strong> <span>All Rights Reserved.</span>""",
