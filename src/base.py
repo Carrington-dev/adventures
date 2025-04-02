56380a7b413325c303be44bba8ef7b66f0e0591e
Carrington Muleya <79579279+Carrington-dev@users.noreply.github.com>
2025-04-02 16:55:05 +0200

    Update base.py
    Co-Authored-By: Stemgon <165911512+stemgon@users.noreply.github.com>
    Co-Authored-By: Carrington Muleya <79579279+carrington-dev@users.noreply.github.com>

--- src/base.py
+++ src/base.py
@@ -3,13 +3,24 @@ from flasgger import Swagger
 
 app = Flask(__name__, static_url_path="/static", static_folder="../static", template_folder='../templates')
 
-app.config['SWAGGER'] = {
-    'title': 'TravelEase API',
-    'uiversion': 3,  # Use Swagger UI version 3
-    'description': 'The ultimate travel booking and management solution',
-    'version': '1.0.0'
+# app.config['SWAGGER'] = {
+#     'title': 'TravelEase API',
+#     'uiversion': 3,  # Use Swagger UI version 3
+#     'description': 'The ultimate travel booking and management solution',
+#     'version': '1.0.0'
+# }
+
+template = {
+    "swagger": "2.0",
+    "info": {
+        "title": "TravelEase API",
+        "description": "The ultimate travel booking and management solution",
+        "version": "1.0.0",
+    },
+    "schemes": ["http", "https"],
 }
 
-swagger = Swagger(app)  # Initialize Swagger
+swagger = Swagger(app, template=template)
+
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///mydatabase.db'  # Using SQLite
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False  # Optional but recommended
