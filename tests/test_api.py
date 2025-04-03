23c3350f09c653cacc687002ad72767fc43fbfd1
Carrington Muleya <79579279+Carrington-dev@users.noreply.github.com>
2025-04-03 15:24:45 +0200

    Update test_api.py
    Co-Authored-By: Stemgon <165911512+stemgon@users.noreply.github.com>
    Co-Authored-By: Carrington Muleya <79579279+carrington-dev@users.noreply.github.com>

--- tests/test_api.py
+++ tests/test_api.py
@@ -12,10 +12,3 @@ def test_home(client):
     response = client.get('/api/home')
     assert response.status_code == 200
     assert response.get_json() == {'message': 'You are now subscribed to our newsletter!.'}
-
-def test_home(client):
-    response = client.get('/api/about')
-    assert response.status_code == 200
-    assert response.get_json() == {
-        "message": "About Us"
-    }
